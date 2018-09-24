9072213c517e0b09fabb97d8509e0bb28cab2b7b
Iqan Shaikh <Iqan.Shaikh@asos.com>
2018-09-24 12:50:17 +0100

    basic implementation done

--- AsbMessageReceiverDemo/Program.cs
+++ AsbMessageReceiverDemo/Program.cs
@@ -1,11 +1,17 @@
-﻿using System;
-
-namespace AsbMessageReceiverDemo
+﻿namespace AsbMessageReceiverDemo
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
+            using (var singleMessageReceiver = new SingleMessageReceiver<ItemModel>())
+            {
+                var item = singleMessageReceiver.ReceiveMessage();
+                Console.WriteLine($"Item - Id: {item.Id}, Name: {item.Name}");
+            }
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
---
+++ AsbMessageReceiverDemo/SingleMessageReceiver.cs
@@ -0,0 +1,78 @@
+﻿namespace AsbMessageReceiverDemo
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Newtonsoft.Json;
+
+    public class SingleMessageReceiver<T> : IDisposable where T : class
+    {
+        private SubscriptionClient subscriptionClient;
+        private bool messageReceived = false;
+        private T received;
+        private readonly string subscriptionName = "asos.legacy.retail.pricing.endpoint";
+        private readonly string topicName = "pricechanges.iqan";
+        private readonly string connectionString = "Endpoint=sb://asos29-test-eun-legacyretail-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=V98cZQMfkhwlqwN9KtqbhMFEAqDwEqMtxK3ZYfi8YBs=";
+
+        private event EventHandler<bool> messageReceivedEvent;
+
+        public SingleMessageReceiver()
+        {
+            Console.WriteLine("Creating subscription client.");
+            this.subscriptionClient = new SubscriptionClient(connectionString, topicName, subscriptionName);
+
+            Console.WriteLine("Listening to message received event.");
+            this.messageReceivedEvent += Receiver_messageReceivedEvent;
+        }
+
+        public T ReceiveMessage()
+        {
+            Console.WriteLine("Registering message handler.");
+            this.subscriptionClient.RegisterMessageHandler(MessageHandler, ErrorHandler);
+            WaitTillMessageIsReceived();
+            return received;
+        }
+
+        private Task MessageHandler(Message message, CancellationToken ct)
+        {
+            Console.WriteLine("Message received");
+            var content = Encoding.UTF8.GetString(message.Body);
+            this.received = JsonConvert.DeserializeObject<T>(content);
+            this.messageReceivedEvent?.Invoke(this, true);
+            return Task.CompletedTask;
+        }
+
+        private Task ErrorHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine("An error occurred.");
+            Console.WriteLine("Exception:" + arg.Exception.Message);
+            return Task.CompletedTask;
+        }
+
+        private void WaitTillMessageIsReceived()
+        {
+            while (!messageReceived)
+            {
+                Console.WriteLine("Message not received. waiting...");
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void Receiver_messageReceivedEvent(object sender, bool e)
+        {
+            Console.WriteLine("Message received event occurred.");
+            this.messageReceived = true;
+        }
+
+        public void Dispose()
+        {
+            if (!this.subscriptionClient.IsClosedOrClosing)
+            {
+                Console.WriteLine("Closing client.");
+                this.subscriptionClient.CloseAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
